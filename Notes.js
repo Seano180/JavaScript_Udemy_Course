2d7c41f00b8952e0aa66db10f107ba560991f478
Seano180 <sean.sheridan22@gmail.com>
2024-02-08 19:57:39 +1100

    Seans JavaScript notes - GitHub version

---
+++ Notes.js
@@ -0,0 +1,17 @@
+// Basic Operators - Section 1, Lesson 13
+
+//Assignment Operators
+/* 
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 (101)
+x--; // x = x - 1 (100)
+*/
+
+// Comparison Operators
+// ** = to the power of; 
+// > = greater than;
+// < = less than; 
+// >= greater than or equal to;
+// <= less than or equal to;
