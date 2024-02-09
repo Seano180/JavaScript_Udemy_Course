1f55cb2ca544499f926ab124669caadee1b33fe1
Seano180 <sean.sheridan22@gmail.com>
2024-02-09 13:44:52 +1100

    Updated notes.js

--- Notes.js
+++ Notes.js
@@ -15,3 +15,19 @@ x--; // x = x - 1 (100)
 // < = less than; 
 // >= greater than or equal to;
 // <= less than or equal to;
+
+
+// Challenge #1 - Section 1, lesson 16
+/* Write your code below. Good luck! 🙂 
+👉 const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIJohn, BMIMark);
+
+const markHigherBMI = (BMIMark > BMIJohn);
+console.log(markHigherBMI); 🔚*/
