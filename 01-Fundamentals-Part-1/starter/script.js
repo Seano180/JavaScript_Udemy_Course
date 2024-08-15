f2e570d8424d227c8c361600ebe79cab58b9042b
Sean Sheridan <seano180@192-168-1-142.tpgi.com.au>
2024-08-15 19:37:34 +1000

    Fundamentals Part 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,4 @@
-let js = "amazing"
-console.log(40 + 8 + 23 - 10);
+let js = "amazing";
+if (js === "amazing") alert("Javascript is fun");
 
-let myFirstName = "Seano"
-console.log(myFirstName)
+console.log(21 + 22 + 19);
