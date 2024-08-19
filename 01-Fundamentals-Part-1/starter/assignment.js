6786e1f31df08b456be8ff4e885d15a4eeac1afa
Sean Sheridan <seano180@192-168-1-142.tpgi.com.au>
2024-08-19 19:28:48 +1000

    Lecture 10 - Values and Variables assignment

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -2,11 +2,10 @@
 // Declare variables called country, continent and population and assign their values according to your own country (population in millions).
 
 // Log their values to the console.
+let country = "Australia";
+let continent = "Oceana";
+let population = "25,000,000";
 
-let myCountry = "Australia";
-let myContinent = "Australia";
-let myPopulation = "26,000,000";
-
-console.log(myCountry)
-console.log(myContinent)
-console.log(myPopulation)
+console.log(country);
+console.log(continent);
+console.log(population);
