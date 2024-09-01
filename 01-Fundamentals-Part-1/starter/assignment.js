c933238694b5260cc1c49a06ec57ee4d9b46cb4d
@Seano180 <sean.sheridan22@gmail.com>
2024-09-01 11:28:07 +1000

    Tutorial 18 - learning about if and else statements

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -96,3 +96,14 @@ console.log(markHigherBMI);
 console.log(
   `${country} is in ${continent} and it's ${population} million people speak ${language}`
 );
+
+// **** Taking Decisions: If / Else Statements - Tutorial 18 ****
+// If your country's population is greater than 33 million, log a string like this to the console: "Portugal's population is 22 million below average" (the 22 is the average of 33 minus the country's population).
+
+// After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original.
+if (population > 33000000) {
+  const popRemaining = 33000000 - population;
+  console.log(`Australia's population is ${popRemaining} below average`);
+} else {
+  console.log("Your population must continue to grow");
+}
