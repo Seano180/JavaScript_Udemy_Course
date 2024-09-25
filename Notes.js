47b29cd087a8a5b16e5d7facf2f1cd8d459d219f
@Seano180 <sean.sheridan22@gmail.com>
2024-09-26 09:19:34 +1000

    Tutorial 58 - Thinking like a Developer

--- Notes.js
+++ Notes.js
@@ -10,8 +10,14 @@ x--; // x = x - 1 (100)
 */
 
 // Comparison Operators
-// ** = to the power of; 
+// ** = to the power of;
 // > = greater than;
-// < = less than; 
+// < = less than;
 // >= greater than or equal to;
 // <= less than or equal to;
+
+// *** Tutorial 58 - Think and Solve problems like a developer ****
+// 1) Understand the problem - ask the right questions
+// 2) Break big problems in to smaller problems a.k.a milestones
+// 3) Dont be afraid to do as much research as we have to e.g. using StackOverflow, MDN docs, Google etc.
+// 4) For bigger problems, write pseudo-code before writing the actual code.
