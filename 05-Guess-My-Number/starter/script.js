29fc843fad35a09774be7e906f43c828ed095f74
@Seano180 <sean.sheridan22@gmail.com>
2024-10-03 14:57:05 +1000

    Tutorial 72 - Manipulating DOM Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,4 +4,12 @@
 // # for ID
 
 console.log(document.querySelector('.message').textContent);
-//**ß */
+
+document.querySelector('.message').textContent = 'Correct Number 🎉';
+
+document.querySelector('.number').textContent = '13';
+document.querySelector('.score').textContent = '1';
+
+document.querySelector('.guess').value = '22';
+
+console.log(document.querySelector('.guess').value);
