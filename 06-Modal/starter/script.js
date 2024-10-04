052a5757bebcb33144543ed04083b82e00dadeb2
@Seano180 <sean.sheridan22@gmail.com>
2024-10-04 20:02:37 +1000

    Tutorial 79 - Project 2 - Modal Window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-model');
+
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+console.log(btnShowModal);
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  console.log(btnShowModal[i].textContent);
+}
