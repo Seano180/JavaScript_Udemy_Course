cea5b5a33c3428b248162390433a39de00290952
@Seano180 <sean.sheridan22@gmail.com>
2024-10-14 10:44:55 +1100

    Tutorial 93 - Scoping in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,22 +1,47 @@
 'use strict';
 
 // **** Tutorial 92 - Scope and the Scope Chain ****
-const a = 'Seano';
-first();
+// const a = 'Seano';
+// first();
 
-function first() {
-  const b = 'hello';
-  second();
+// function first() {
+//   const b = 'hello';
+//   second();
 
-  function second() {
-    const c = 'hi';
-    third();
+//   function second() {
+//     const c = 'hi';
+//     third();
+//   }
+// }
+
+// function third() {
+//   const d = 'hey';
+//   console.log(d + c + b + a);
+// }
+
+// **** Tutorial 93 - Scoping in Practice ****
+
+function calcAge(birthYear) {
+  const age = 2024 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age} born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const string = `Oh, and you're a millenial, ${firstName}`;
+      console.log(string);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(add(2, 3)); // this will work if we turn of "Use Strict"
   }
+  printAge();
+
+  return age;
 }
 
-function third() {
-  const d = 'hey';
-  console.log(d + c + b + a);
-}
-
-// **** Updates ****
+const firstName = 'Seano';
+calcAge(1987);
