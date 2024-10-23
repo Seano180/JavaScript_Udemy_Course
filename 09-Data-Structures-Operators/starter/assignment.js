cd2e727fcec7c4abdd09c5a02aa107b3a3cecf08
@Seano180 <sean.sheridan22@gmail.com>
2024-10-23 19:31:31 +1100

    Tutorial 113 - Enhanced Object Literals

--- 09-Data-Structures-Operators/starter/assignment.js
+++ 09-Data-Structures-Operators/starter/assignment.js
@@ -480,3 +480,29 @@ for (const [index, author] of allAuthors.entries()) {
 }
 
 // **** Enhanced Object Literals - Tutorial 113 ****
+
+// 9.1
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+console.log(newBook);
+
+// 9.2
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages,
+};
+
+console.log(newBook2);
