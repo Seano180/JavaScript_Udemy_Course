7fd6c07c94f3b74edbe70961d4b3952db6575523
@Seano180 <sean.sheridan22@gmail.com>
2024-11-03 21:50:22 +1100

    Tutorial 119 - Maps Iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -333,4 +333,33 @@ console.log(staffSet1);
 // using sets to determine size using the ".size" command
 console.log(new Set('SeanoSheridan').size);
 
-// **** Tutorial 118 - Maps: Fundamentals ****
+// **** Tutorial 119 - Maps: Iteration ****
+const question = new Map([
+  ['question', 'what is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['Correct', 3],
+  [true, 'Correct 😎'],
+  [false, 'Incorrect 😭'],
+]);
+console.log(question);
+
+// Convert Object to Map
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key} : ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('Correct') === answer));
+
+// Convert a Map back to an Array
+// we can do this using the spread operator
+console.log([...question]);
